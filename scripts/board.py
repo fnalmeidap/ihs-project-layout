bb959f064a00380a803dcc4415e1ad3f1e81669d
fnalmeidap <fnap@cin.ufpe.br>
2022-05-11 09:02:56 -0300

    fix: typo in if

--- scripts/board.py
+++ scripts/board.py
@@ -7,9 +7,9 @@ from fcntl import ioctl
 from ioctl_cmds import *
 
 if len(sys.argv) < 2:
-print("Error: expected more command line arguments")
-print("Syntax: %s </dev/device_file>"%sys.argv[0])
-exit(1)
+    print("Error: expected more command line arguments")
+    print("Syntax: %s </dev/device_file>"%sys.argv[0])
+    exit(1)
 
 fd = os.open(sys.argv[1], os.O_RDWR)
 
