47ab2eecefbffa1701989cb1466cee3c07485645
fnalmeidap <fnap@cin.ufpe.br>
2022-05-11 08:57:20 -0300

    fix: file context

--- scripts/app.py
+++ scripts/app.py
@@ -4,12 +4,13 @@ from fcntl import ioctl
 from ioctl_cmds import *
 from board import DE2i
 
+if len(sys.argv) < 2:
+print("Error: expected more command line arguments")
+print("Syntax: %s </dev/device_file>"%sys.argv[0])
+exit(1)
+
+fd = os.open(sys.argv[1], os.O_RDWR)
+
 if __name__ == "__main__":
 
-    if len(sys.argv) < 2:
-    print("Error: expected more command line arguments")
-    print("Syntax: %s </dev/device_file>"%sys.argv[0])
-    exit(1)
-
-    fd = os.open(sys.argv[1], os.O_RDWR)
     board = DE2i(fd)
